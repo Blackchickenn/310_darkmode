6c514af5e31407dc227cd34bd789b20caa1938df
Blackchickenn <marcel.madaj@gmail.com>
2022-03-29 09:26:31 +0200

    fixed nav-menu and in responsive and default design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,25 +11,16 @@ export default function Navbar(props){
                 </ul>
             </div>
                 <div className="burger--cross">
-                    <i className="fa-solid fa-bars " onClick={props.displayBurgerMenu}></i> 
-                    <i className="fa-solid fa-xmark"></i>
+                    <div className={props.burgerMenu ? "" : "nohamburger"}>
+                        <i className="fa-solid fa-bars " onClick={props.displayBurgerMenu}></i> 
+                    </div>
+                    <div className={props.burgerMenu ? "" : "cross"}>
+                        <i className="fa-solid fa-xmark" onClick={props.displayBurgerMenu}></i>
+                    </div>
                 </div>
         </nav>
     )
 }
 
 
-// export default function Navbar(props){
-//     return(
-//         <nav className={props.darkMode ? "dark" : ""}>
-//                 <ul className={props.darkMode ? "dark" : ""}>
-//                     <li>First</li>
-//                     <li>Second</li>
-//                     <li>Third</li>
-//                 </ul>
-//                 <div className={props.burgerMenu ? "burger" : "noburger"}>
-//                     <i className="fa-solid fa-bars " onClick={props.displayBurgerMenu}></i> 
-//                 </div>
-//         </nav>
-//     )
-// }
+
